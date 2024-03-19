eb28f5d3d716cd60c20823c65a5d28f2602d6e87
VingeRay <vingeray@gmail.com>
2024-03-19 15:55:22 +0000

    save

--- src/libs/twitter.ts
+++ src/libs/twitter.ts
@@ -17,7 +17,12 @@ type FetchTweetResponse = {
   tweet: string
 }
 
-const client = new TwitterApi(TWITTER_AUTH_BEARER_TOKEN)
+const client = new TwitterApi({
+  appKey: TWITTER_CONSUMER_KEY,
+  appSecret: TWITTER_CONSUMER_SECRET,
+  accessToken: TWITTER_ACCESS_TOKEN_KEY,
+  accessSecret: TWITTER_ACCESS_TOKEN_SECRET
+})
 
 console.log('client', client)
 
@@ -56,10 +61,10 @@ function expandUrls(urls: TweetEntitiesV2['urls'], text: string): string {
 
 export async function tweet(post: Post) {
   // post to twitter
-  // await client.v2.tweet({
-  //   text: `${post.text ?? ''} \nvia: ${post.url}`,
-  // })
-  await client.v1.tweet(`${post.text ?? ''} \nvia: ${post.url}`)
+  await client.v2.tweet({
+    text: `${post.text ?? ''} \nvia: ${post.url}`,
+  })
+  // await client.v1.tweet(`${post.text ?? ''} \nvia: ${post.url}`)
 }
 
 export async function fetchLatestTweet(): Promise<FetchTweetResponse | void> {
