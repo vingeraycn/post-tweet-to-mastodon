cde413852f2028142ce738fd6da7e36217aaeddc
VingeRay <vingeray@gmail.com>
2024-03-20 09:32:22 +0800

    update mastodon-to-twitter.yml, credentials.ts and twitter.ts

--- src/config/credentials.ts
+++ src/config/credentials.ts
@@ -12,7 +12,8 @@ export const TWITTER_ACCESS_TOKEN_KEY =
   process.env['TWITTER_ACCESS_TOKEN_KEY'] ?? ''
 export const TWITTER_ACCESS_TOKEN_SECRET =
   process.env['TWITTER_ACCESS_TOKEN_SECRET'] ?? ''
-export const TWITTER_AUTH_BEARER_TOKEN = process.env['TWITTER_AUTH_BEARER_TOKEN'] ?? ''
+export const TWITTER_AUTH_BEARER_TOKEN =
+  process.env['TWITTER_AUTH_BEARER_TOKEN'] ?? ''
 
 /**
  * How to get your Mastodon credentials:
